f8ca13c78b1deb81349816abadfc24a5ec9cfe7f
871246995 <871246995@qq.com>
2018-04-11 17:35:57 +0800

    播放器

--- simple-vue-project/src/App.vue
+++ simple-vue-project/src/App.vue
@@ -1,60 +1,83 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg+msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <!-- 这是唱碟的背景 -->
+    <div class="bg-blur"></div>
+    <!-- 这是会旋转的唱碟 -->
+    <div class="dist">
+      <div class="song-img"></div>
+
+    </div>
+    <!-- 播放控制条 -->
+    <div class="ctr-box"></div>
+    <!-- 右侧的部分歌词标题 -->
+    <div class="right-part"></div>
+    <!-- 这是播放进度条 -->
+    <div class="progress"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
+<style>
+  #app,.bg-blur{
+    margin: 0;
+    padding: 0;
+    top:50%;
+    left:50%;
+    min-width: 922px;
+    height: 667px;
+    background-color: white;
+    position: fixed;
+    transform: translate(-50%,-50%);
+    -ms-transform: translate(-50%,-50%);
+    -webkit-transform: translate(-50%,-50%);
+    -ms-transform: translate(-50%,-50%);
+    -o-transform: translate(-50%,-50%);
+  }
+  #app{
+    background: rgba(0, 0, 0, .8);
+  }
+  .bg-blur{
+    border: 1px solid gray;
+    z-index: -1;
+    background-image: url("http://pic.xiami.net/images/album/img60/1260/21003502061466736281_1.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter: blur(20px);
+  }
+  .dist{
+    width: 300px;
+    height: 300px;
+    position: relative;
+    left: 10%;
+    top: 10%;
+    background-image: radial-gradient(5em 30em ellipse, #fff, #000);
+    border: 2px solid #131313;
+    border-radius: 150px;
+    box-shadow: 0 0 0 10px #343935;
+    opacity: .7;
+    transform:rotate(45deg);
+  }
+  .song-img{
+    height: 200px;
+    width: 200px;
+    border-radius: 100px;
+    background: url("http://pic.xiami.net/images/album/img60/1260/21003502061466736281_1.jpg") center no-repeat;
+    background-size: cover;
+    position: relative;
+    left: 50px;
+    top: 50px;
+    border: 2px solid #131313;
+  }
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
